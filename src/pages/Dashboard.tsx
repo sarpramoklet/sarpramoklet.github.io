ebd344a6b5ae8313e615c6ece3746daf1632e3a2
hadhiee <hadhiee@gmail.com>
2026-03-31 12:34:51 +0700

    Update: Updated all API URLs with latest Google Apps Script version

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import { Activity, Clock10, CheckSquare, TriangleAlert, ArrowUpRight, ArrowDownR
 import { getCurrentUser, ROLES } from '../data/organization';
 import { getUtilityChartData } from '../data/utilities';
 
-const FINANCE_API_URL = "https://script.google.com/macros/s/AKfycbyDoWkbsahg_JmUKQNFvydy9Mlx4yScY4yp6pGDPkNtfI9V8KBDuzZhte_lSJObuFtiEg/exec";
+const FINANCE_API_URL = "https://script.google.com/macros/s/AKfycbyiQcb0i1TRlhPgXSXks1SxEYSfRgk-PkFOKuoJn1hK-en708kYHMiYZqBZ1JhZuwROBg/exec";
 
 const areaData = [
   { name: 'Jan', IT: 400, Lab: 240, Sarpras: 240 },
